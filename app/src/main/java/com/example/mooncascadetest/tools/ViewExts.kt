28d05063a0b6b32c7ebc027386cb42f8e57fbc78
Stanislav Sobol <stanislav.i.sobol@gmail.com>
2021-06-01 16:40:00 +0300

    Refactoring. Redundant resources

--- app/src/main/java/com/example/mooncascadetest/tools/ViewExts.kt
+++ app/src/main/java/com/example/mooncascadetest/tools/ViewExts.kt
@@ -1,10 +1,14 @@
 package com.example.mooncascadetest.tools
 
+import android.graphics.Paint
 import android.widget.TextView
 import androidx.core.view.isVisible
 
-fun TextView.setTextWithVisibility(text: String) {
+fun TextView.setTextWithVisibility(text: String, underlined: Boolean = false) {
     if (text.isNotBlank()) {
+        if (underlined) {
+            paintFlags = paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        }
         this.text = text
     }
     isVisible = text.isNotBlank()
